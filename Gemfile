ac04833f56354d85bb1c8c19622475a62ade916a
gabrielfel1x <gabrielfelxx@gmail.com>
2025-01-22 23:18:05 -0300

    action generate PDF using prawn gem

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source "https://rubygems.org"
 
 gem 'prawn-rails', '~> 0.1.1'
+
+gem 'solargraph', '~> 0.1.0'
+
+gem 'rubocop', '~> 0.42.0'
--- generator.rb
+++ generator.rb
@@ -19,8 +19,10 @@ def collect_data
     print "The URL of your LinkedIn profile: "
     data[:linkedin] = gets.chomp
 
-    data[:experiences] = []
     puts "Add your work experiences (leave blank to finish):"
+    data[:summary] = gets.chomp
+    
+    data[:experiences] = []
 
     loop do
         print "Job title: "
@@ -45,6 +47,47 @@ def collect_data
 
         data[:skills] << skill
     end
+    
+    print "Now choose a name for your resume file: "
+    data[:file_name] = gets.chomp
 
     data
 end
+
+def generate_pdf(data)
+    Prawn::Document.generate("#{data[:file_name]}.pdf") do |pdf|
+        pdf.text data[:name], size: 18, style: :bold
+        pdf.text "#{data[:email]} | #{data[:phone]}", size: 12, style: :italic
+        pdf.move_down 20
+        
+        pdf.stroke_horizontal_rule
+        pdf.move_down 10
+        pdf.text "Professional Summary", size: 16, style: :bold
+        pdf.text data[:summary], size: 12, align: :justify
+        pdf.move_down 20
+        
+        pdf.stroke_horizontal_rule
+        pdf.move_down 10
+        pdf.text "Work Experiences", size: 16, style: :bold
+        data[:experiences].each do |exp|
+            pdf.text "#{exp[:title]} - #{exp[:company]}", size: 14, style: :bold
+            pdf.text exp[:description], size: 12, align: :justify
+            pdf.move_down 10
+        end
+        pdf.move_down 20
+    
+        pdf.stroke_horizontal_rule
+        pdf.move_down 10
+        pdf.text "Skills", size: 16, style: :bold
+        pdf.move_down 5
+        data[:skills].each do |skill|
+        pdf.text "- #{skill}", size: 12
+        end
+    end
+
+    puts "Resume generated successfully: #{data[:file_name]}.pdf"
+end
+
+
+user_data = collect_data
+generate_pdf(user_data)
